986a5154bef5c52ce75aaa029b55c5d33482f0c0
sebastianhawlitzki <business.hawlitzki@gmail.com>
2023-01-25 11:19:31 +0100

    feat: created home- and transactionpage + routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import TransactionPage from "./pages/TransactionPage";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<HomePage/>}/>
+                <Route path="/transaction" element={<TransactionPage/>}/>
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
