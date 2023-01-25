8deee00491fd3def7ccbedab2ff9a1b5f35e8fbe
sebastianhawlitzki <business.hawlitzki@gmail.com>
2023-01-25 11:49:45 +0100

    feat: Login frontend implementation + logout , bugfix on package.json

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,13 @@
 import React from 'react';
 import './App.css';
-import {BrowserRouter, Route, Routes} from "react-router-dom";
-import HomePage from "./pages/HomePage";
-import TransactionPage from "./pages/TransactionPage";
+import {BrowserRouter} from "react-router-dom";
+import Root from "./Root";
+
 
 export default function App() {
     return (
         <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<HomePage/>}/>
-                <Route path="/transaction" element={<TransactionPage/>}/>
-            </Routes>
+            <Root/>
         </BrowserRouter>
     );
 }
