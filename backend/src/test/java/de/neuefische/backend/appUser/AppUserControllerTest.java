b889ee3a40450a68f1d57b793835658d4a6e05fd
sebastianhawlitzki <business.hawlitzki@gmail.com>
2023-01-26 22:28:25 +0100

    feat: integration test on all endpoints - passed

--- backend/src/test/java/de/neuefische/backend/appUser/AppUserControllerTest.java
+++ backend/src/test/java/de/neuefische/backend/appUser/AppUserControllerTest.java
@@ -5,11 +5,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-
 
 
 @SpringBootTest
@@ -50,6 +50,85 @@ class AppUserControllerTest {
 
     }
 
+    @Test
+    void create_whenAppUserAlreadyExists_shouldReturnConflict() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/app-users")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("""
+                        {
+                            "id": "1",
+                            "username": "user",
+                            "password": "password",
+                            "accountType": "private",
+                            "accountBalance": 0
+                        }
+                        """)).andExpectAll(
+                MockMvcResultMatchers.status().isOk(),
+                MockMvcResultMatchers.content().json("""
+                                  {
+                                    "id": "1",
+                                    "username": "user",
+                                    "password": "",
+                                    "accountNumber": 1,
+                                    "accountType": "private",
+                                    "accountBalance": 0
+                                }
+                                """,
+                        true
+                )
+        );
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/app-users")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("""
+                        {
+                            "id": "1",
+                            "username": "user",
+                            "password": "password",
+                            "accountType": "private",
+                            "accountBalance": 0
+                        }
+                        """)).andExpectAll(
+                MockMvcResultMatchers.status().isConflict());
 
+    }
+
+    @Test
+    void login_whenAppUserNotExist_thenReturn401() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/app-users/login")).
+                andExpectAll(MockMvcResultMatchers.status().isUnauthorized());
+    }
+
+    @Test
+    @WithMockUser(username = "user", roles = "BASIC")
+    void login_whenAppUserExist_thenReturn200() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/app-users/login"))
+                .andExpectAll(MockMvcResultMatchers.status().isOk());
+    }
+
+    @Test
+    void me_whenAppUserNotLoggedIn_thenReturn401() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/app-users/me")).
+                andExpectAll(MockMvcResultMatchers.status().isUnauthorized());
+    }
+
+    @Test
+    @WithMockUser(username = "user", roles = "BASIC")
+    void me_whenAppUserNotLoggedIn_thenReturn200() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/app-users/me")).
+                andExpectAll(MockMvcResultMatchers.status().isOk());
+    }
+
+    @Test
+    void logout_whenAppUserNotLoggedInAndLogsOut_shouldReturn401() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/app-users/logout"))
+                .andExpectAll(MockMvcResultMatchers.status().isUnauthorized());
+    }
+
+    @Test
+    @WithMockUser(username = "user", roles = "BASIC")
+    void logout_whenAppUserLoggedInAndLogsOut_shouldReturn200() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/app-users/logout"))
+                .andExpectAll(MockMvcResultMatchers.status().isOk());
+    }
 
 }
