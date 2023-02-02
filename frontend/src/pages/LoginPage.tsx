1063801d5f10c9ff365f0ba6c6565cf69a6001cb
sebastianhawlitzki <business.hawlitzki@gmail.com>
2023-02-02 19:08:29 +0100

    feat: added navbar to LoginPage

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -6,14 +6,16 @@ import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
 import Link from '@mui/material/Link';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+
 
 
 export default function LoginPage () {
@@ -62,10 +64,25 @@ function Copyright(props: any) {
 
 
     return (
+        <Box sx={{flexGrow: 1}}>
+            <AppBar position="static">
+                <Toolbar>
+                    <IconButton
+                        size="large"
+                        edge="start"
+                        color="inherit"
+                        aria-label="menu"
+                        sx={{mr: 2}}>
+                    </IconButton>
+                    <Typography variant="h5" component="div" sx={{flexGrow: 1}}>
+                        SmartBanking
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+            {error && <div>{error}</div>}
+            <CssBaseline />
 
             <Container component="main" maxWidth="xs">
-                {error && <div>{error}</div>}
-                <CssBaseline />
                 <Box
                     sx={{
                         marginTop: 8,
@@ -74,7 +91,7 @@ function Copyright(props: any) {
                         alignItems: 'center',
                     }}
                 >
-                    <Avatar sx={{ m: 1, bgcolor: 'success.main' }}>
+                    <Avatar sx={{ m: 1, bgcolor: 'primary' }}>
                         <LockOutlinedIcon />
                     </Avatar>
                     <Typography component="h1" variant="h5">
@@ -103,34 +120,23 @@ function Copyright(props: any) {
                             autoComplete="current-password"
                             onChange={e => setPassword(e.target.value)}
                         />
-                        <FormControlLabel
-                            control={<Checkbox value="remember" color="primary" />}
-                            label="Remember me"
-                        />
                         <Button
                             type="submit"
                             fullWidth
                             variant="contained"
-                            sx={{ mt: 3, mb: 2, bgcolor: 'success.dark' }}
+                            sx={{ mt: 3, mb: 2, bgcolor: 'primary' }}
                         >
                             Sign In
                         </Button>
-                        <Grid container>
-                            <Grid item xs>
-                                <Link href="#" variant="body2">
-                                    Forgot password?
-                                </Link>
-                            </Grid>
-                            <Grid item>
+                        <Grid container justifyContent="center">
                                 <Link href="#" variant="body2">
                                     {"Don't have an account? Sign Up"}
                                 </Link>
-                            </Grid>
                         </Grid>
                     </Box>
                 </Box>
                 <Copyright sx={{ mt: 8, mb: 4 }} />
             </Container>
-
+        </Box>
     );
 }
