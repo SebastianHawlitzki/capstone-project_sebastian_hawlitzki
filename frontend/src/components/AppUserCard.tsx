2103d68d44aed27dfe956ad7b40d7fabddf48c0a
sebastianhawlitzki <business.hawlitzki@gmail.com>
2023-02-05 21:50:47 +0100

    feat: styling and implement accountBalance on transactionPage

--- frontend/src/components/AppUserCard.tsx
+++ frontend/src/components/AppUserCard.tsx
@@ -18,57 +18,75 @@ export default function AppUserCard() {
     const card = (
         <React.Fragment>
             <CardContent>
-                <Typography variant="h5" component="div" textAlign="center" padding={1}
-                            style={{fontSize: 23}}>
-                    Aktuelle Konto-Übersicht
+                <Typography variant="h5" component="div" textAlign="center" paddingBottom={1}
+                            style={{fontSize: 22}}>
+                    Aktuelle Kontoübersicht
                 </Typography>
                 <Divider/>
                 <Typography sx={{mb: 1.5}} color="text.secondary" paddingTop={1}>
                     {appUser.username}
                 </Typography>
-                <Typography variant="body1" paddingTop={2} paddingBottom={2}
-                            style={{fontSize: 18, fontWeight: 400}}>
-                    IBAN: DE 99 9009 9009 0000 0{appUser.accountNumber}
-                </Typography>
-                <Divider/>
                 <Grid container justifyContent={"space-between"}>
                     <Grid item>
                         <Typography variant="body1" paddingTop={2} paddingBottom={2}
                                     style={{fontSize: 18, fontWeight: 400}}>
-                            Kontostand:
+                            IBAN:
                         </Typography>
                     </Grid>
                     <Grid item>
-                        <Typography variant="body1" paddingTop={2} paddingBottom={2}
-                                    style={{fontSize: 18, fontWeight: 400}}>
-                            {appUser.accountBalance}€
-                        </Typography>
+                    <Typography variant="body1" paddingTop={2} paddingBottom={2}
+                                style={{fontSize: 18, fontWeight: 400}}>
+                        DE 99 9009 9009 0000 0{appUser.accountNumber}
+                    </Typography>
                     </Grid>
                 </Grid>
-                <Divider/>
-                <Grid container justifyContent={"space-between"}>
-                    <Grid item>
-                        <Typography variant="body1" paddingTop={2}
-                                    style={{fontSize: 18, fontWeight: 400}}>
-                            Konto-Typ:
-                        </Typography>
+                    <Divider/>
+                    <Grid container justifyContent={"space-between"}>
+                        <Grid item>
+                            <Typography variant="body1" paddingTop={2} paddingBottom={2}
+                                        style={{fontSize: 18, fontWeight: 400}}>
+                                Kontostand:
+                            </Typography>
+                        </Grid>
+                        <Grid item>
+                            <Typography variant="body1" paddingTop={2} paddingBottom={2}
+                                        style={{fontSize: 18, fontWeight: 400}}>
+                                {appUser.accountBalance}€
+                            </Typography>
+                        </Grid>
                     </Grid>
-                    <Grid item>
-                        <Typography variant="body1" paddingTop={2}
-                                    style={{fontSize: 18, fontWeight: 400}}>
-                            {appUser.accountType}
-                        </Typography>
+                    <Divider/>
+                    <Grid container justifyContent={"space-between"}>
+                        <Grid item>
+                            <Typography variant="body1" paddingTop={2}
+                                        style={{fontSize: 18, fontWeight: 400}}>
+                                Kontoart:
+                            </Typography>
+                        </Grid>
+                        <Grid item>
+                            <Typography variant="body1" paddingTop={2}
+                                        style={{fontSize: 18, fontWeight: 400}}>
+                                {appUser.accountType}
+                            </Typography>
+                        </Grid>
                     </Grid>
-                </Grid>
 
             </CardContent>
         </React.Fragment>
-    );
+);
 
 
-    return (
-        <Box sx={{minWidth: 275, display: 'flex', alignItems: 'center', justifyContent: 'center', marginLeft:2, marginTop:6, marginRight:2,}}>
-            <Card variant="outlined">{card}</Card>
-        </Box>
-    );
+return (
+    <Box sx={{
+        minWidth: 275,
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginLeft: 2,
+        marginTop: 6,
+        marginRight: 2,
+    }}>
+        <Card variant="outlined">{card}</Card>
+    </Box>
+);
 }
--- frontend/src/pages/TransactionPage.tsx
+++ frontend/src/pages/TransactionPage.tsx
@@ -8,12 +8,14 @@ import * as React from "react";
 import BottomAppBarTransactionPage from "../components/BottomAppBarTransactionPage";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
-import {Stack} from "@mui/material";
+import {Grid, Paper, Stack} from "@mui/material";
 import SendIcon from '@mui/icons-material/Send';
 import Container from "@mui/material/Container";
 import {Transaction} from "../models/Transaction";
 import {FormEvent, useState} from "react";
 import axios from "axios";
+import useAppUser from "../hooks/useAppUser";
+
 
 
 export default function TransactionPage() {
@@ -47,7 +49,10 @@ export default function TransactionPage() {
     };
 
 
-
+    const {appUser} = useAppUser();
+    if (!appUser) {
+        return <div>...</div>;
+    }
 
 
 
@@ -81,7 +86,7 @@ export default function TransactionPage() {
                     <Box
                         component="form"
                         sx={{
-                            '& .MuiTextField-root': {m: 1, width: '25ch'},
+                            '& .MuiTextField-root': {m: 1, width: '35ch'},
                         }}
                         noValidate
                         autoComplete="off"
@@ -126,7 +131,7 @@ export default function TransactionPage() {
                                 justifyContent:'center',
                                 alignItems: 'center',
                             }}>
-                                <Button type="submit" variant="contained" endIcon={<SendIcon/>}>
+                                <Button type="submit" variant="contained" endIcon={<SendIcon/>} style={{width: '35ch'}}>
                                     Überweisen
                                 </Button>
                             </Stack>
@@ -134,6 +139,22 @@ export default function TransactionPage() {
                     </Box>
                 </Box>
             </Container>
+            <Box sx={{
+                display: 'flex',
+                justifyContent:'center',
+                alignItems: 'center',
+            }}>
+            <Paper elevation={1} style={{ padding: 10, margin:20, marginTop:50, width: '35ch'}}>
+                <Grid container justifyContent={"space-between"}>
+                    <Grid item>
+                <Typography variant="h6" style={{fontSize: 18, fontWeight: 400}}>Kontostand:</Typography>
+                </Grid>
+                    <Grid item>
+                <Typography variant="h6" style={{fontSize: 18, fontWeight: 400}}>{appUser.accountBalance} €</Typography>
+                </Grid>
+                </Grid>
+            </Paper>
+            </Box>
             <BottomAppBarTransactionPage/>
         </Box>
     );
