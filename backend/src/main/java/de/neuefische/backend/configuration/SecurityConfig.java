07144f851316769749a050a99c0a2c4dea9c54da
sebastianhawlitzki <business.hawlitzki@gmail.com>
2023-02-16 15:04:04 +0100

    fix: security, sonar exclusions of configuration

--- backend/src/main/java/de/neuefische/backend/configuration/SecurityConfig.java
+++ backend/src/main/java/de/neuefische/backend/configuration/SecurityConfig.java
@@ -38,6 +38,7 @@ public class SecurityConfig {
                         "/api/**"
                 )
                 .authenticated()
+                .anyRequest().permitAll()
                 .and()
                 .build();
     }
